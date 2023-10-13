147ab32a9cf8ecd9ef452408f8504419895a77cd
KenaiGonzalezOrtega <113781990+KenaiGonzalezOrtega@users.noreply.github.com>
2023-10-13 11:53:18 +0100

    .gitignore

--- app/src/main/java/com/example/workflow/MainActivity.kt
+++ app/src/main/java/com/example/workflow/MainActivity.kt
@@ -41,6 +41,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     WorkFlowTheme {
-        Greeting("Android")
+        Greeting("Kenai")
     }
 }
