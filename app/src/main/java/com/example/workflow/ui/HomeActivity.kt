d9de69afbb24a2f12e9742ab13d3e8478d830c99
Juan Pereiro González <jpereiro1@gmail.com>
2023-11-11 12:16:08 +0000

    Primera pagina del frontend

--- app/src/main/java/com/example/workflow/ui/HomeActivity.kt
+++ app/src/main/java/com/example/workflow/ui/HomeActivity.kt
@@ -1,4 +1,4 @@
-package com.example.workflow
+package com.example.workflow.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,10 +8,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,17 +17,12 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.workflow.R
 import com.example.workflow.ui.theme.BlueWorkFlow
 import com.example.workflow.ui.theme.GreenWorkFlow
-import com.example.workflow.ui.theme.Pink40
 import com.example.workflow.ui.theme.WorkFlowTheme
 import com.example.workflow.ui.theme.jua
 
