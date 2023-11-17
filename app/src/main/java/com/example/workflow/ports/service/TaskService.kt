313f3e56464d64c1d76ff5ed6d02e8f9cb24da24
Miguel Ortega Abelleira <miguelorab@gmail.com>
2023-11-17 12:27:13 +0000

    Finished Task local and remote repositories and TaskService

--- app/src/main/java/com/example/workflow/ports/service/TaskService.kt
+++ app/src/main/java/com/example/workflow/ports/service/TaskService.kt
@@ -4,30 +4,87 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import com.example.workflow.domain.entities.Task
-import com.example.workflow.ports.repository.TaskRepository
+import com.example.workflow.ports.repository.TaskLocalRepository
+import com.example.workflow.ports.repository.TaskRemoteRepository
+import com.example.workflow.utils.InternetChecker
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.count
+import kotlinx.coroutines.flow.first
+import java.lang.IllegalArgumentException
 import java.util.UUID
 
-class TaskService(private val taskRepository: TaskRepository): Service() {
+class TaskService(private val localRepository: TaskLocalRepository,
+                  private val remoteRepository: TaskRemoteRepository
+): Service() {
 
-    fun getTaskByIdStream(id: UUID): Flow<Task?> {
-        return taskRepository.getByIdStream(id)
+    suspend fun getTaskById(id: UUID): Task {
+        var taskStream: Flow<Task?> = localRepository.getTaskByIdStream(id = id)
+        if (taskStream.count() > 0) {
+            return taskStream.first()?: throw IllegalArgumentException("Not found")
+        }
+
+        taskStream = remoteRepository.getTaskByIdStream(id = id)
+        if (taskStream.count() > 0) {
+            val task: Task? = taskStream.first()
+            if (task != null) {
+                localRepository.insertTask(task, false)
+                return task
+            } else
+                throw InternalError("There was a problem while the object was being found.")
+        } else
+            throw IllegalArgumentException("Not found")
     }
 
-    fun getAllTasksStream(): Flow<List<Task>> {
-        return taskRepository.getAllTasksStream()
+    suspend fun getAllTasks(): List<Task> {
+        return if (InternetChecker.checkConnectivity(this)) {
+            try {
+                val tasksFromRemote = remoteRepository.getAllTasksStream().first()
+                localRepository.saveAll(tasksFromRemote)
+                tasksFromRemote
+            } catch (e: Exception) {
+                localRepository.getAllTasksStream().first() // TODO: Cambiar por el error correspondiente de Firebase
+            }
+        } else {
+            localRepository.getAllTasksStream().first()
+        }
     }
-
     suspend fun createTask(task: Task) {
-        taskRepository.insertTask(task)
+        if (InternetChecker.checkConnectivity(this)) {
+            try {
+                remoteRepository.insertTask(task)
+                localRepository.insertTask(task, false)
+            } catch (e: Exception) {
+                localRepository.insertTask(task, true) // TODO: Cambiar por el error correspondiente de Firebase
+            }
+        } else {
+            localRepository.insertTask(task, true)
+        }
     }
 
     suspend fun deleteTask(task: Task) {
-        taskRepository.deleteTask(task)
+        if (InternetChecker.checkConnectivity(this)) {
+            try {
+                remoteRepository.deleteTask(task)
+                localRepository.deleteTask(task)
+            } catch (e: Exception) {
+                localRepository.deleteTask(task) // TODO: Cambiar por el error correspondiente de Firebase
+            }
+        } else {
+            localRepository.deleteTask(task)
+        }
     }
 
     suspend fun updateTask(task: Task) {
-        taskRepository.updateTask(task)
+        if (InternetChecker.checkConnectivity(this)) {
+            try {
+                remoteRepository.updateTask(task)
+                localRepository.updateTask(task, false)
+            } catch (e: Exception) {
+                localRepository.updateTask(task, true) // TODO: Cambiar por el error correspondiente de Firebase
+            }
+        } else {
+            localRepository.updateTask(task, true)
+        }
     }
 
     override fun onBind(p0: Intent?): IBinder? {
