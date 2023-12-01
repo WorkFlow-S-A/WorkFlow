75cb7fad35c00fb0d73313ef9c1abae84751eb27
Juan Pereiro González <jpereiro1@gmail.com>
2023-12-01 11:58:05 +0000

    Mover MainActivity, empezando con taskUi

---
+++ app/src/main/java/com/example/workflow/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.workflow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.workflow.ui.theme.WorkFlowTheme
+import com.example.workflow.ui.WorkFlowApp
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WorkFlowTheme {
+                WorkFlowApp()
+            }
+
+        }
+    }
+
+
+
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        WorkFlowTheme {
+            WorkFlowApp()
+        }
+    }
+
+
+}
